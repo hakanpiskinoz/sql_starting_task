36cb4b1fd6af98a5b0cf976cf6b47a36d77e4ca3
hakanpiskinoz <40467544+hakanpiskinoz@users.noreply.github.com>
2019-05-09 10:31:00 +0300

    Sql starting tasks
    hakansql.sql : My code for the task solutions with my comments
    test.cvs : Data file on cvs type which we need to import
    test.xlsx : Data file with tasks on this project (tasks are Turkish on this file. You can find in English on the following)
    Preperation:
    -to create and build "create table" with column names.
    -to convert each row of cvs file to insert into
    -to check your data is okay or not (just 1-2 rows)
    Tasks:
    1) Total rows number. Ex: 30
    2) Distinct countries number Ex: 10
    3) How many repeat of all countries. Ex: Afghanistan, 2
    4) Sort data according to number of repetitions of countries. Ex: Aruba, 3
    5) Binary numbers of "size" and "food match". Ex: 75CL, Duck, 3
    6) Starting with "Pe" on the "Grape" column. Ex: Petit Verdot
    7) Output of "Country" and "Size_new". (Size_new: when 75CL is 0,75. When 1L is 1.) Ex: Aruba, 1
    8) Total litre of "Size_new". Ex: 50
    9) Total litre of each contry. Ex: Aruba, 3 | Armenia 0,75

---
+++ hakansql.sql
@@ -0,0 +1,59 @@
+﻿DROP TABLE hakansql
+CREATE TABLE hakansql (
+Country varchar,
+Region varchar,
+Grape varchar,
+Size varchar,
+Sweetness varchar,
+Colour varchar,
+"Food match" varchar
+);
+
+SET CLIENT_ENCODING = 'UTF8';
+
+COPY hakansql(Country,Region,Grape,Size,Sweetness,Colour,"Food match") 
+FROM '‪C:\test.csv' DELIMITER ';' CSV
+
+SELECT * from hakansql          --open table
+
+SELECT COUNT(*) FROM hakansql;  --number of rows (1)
+
+SELECT DISTINCT country         --just distinct rows (2)
+FROM hakansql;
+
+SELECT country, COUNT(*)        --just duplicate countries (3)
+FROM hakansql
+GROUP BY country
+HAVING COUNT(*) > 1
+
+SELECT country, COUNT(country) AS MOST_FREQUENT  --all countries accourding to most duplication in a column (4)
+FROM hakansql
+GROUP BY country
+ORDER BY COUNT(country) DESC
+
+SELECT COUNT(*)AS DUPLICATES_COUNT, size, "Food match" --multiple duplicated rows (5)
+FROM hakansql
+GROUP BY size,"Food match"
+HAVING COUNT(*) > 1
+
+SELECT * FROM hakansql   --data which start with "Pe" on the Grape column
+WHERE Grape LIKE 'Pe%' 
+
+SELECT Country, 	--output of size_new and country (7)
+CAST(CASE Size WHEN '75CL' THEN '0.75' WHEN '1L' THEN '1' 
+END AS DOUBLE PRECISION) AS Size_new 
+FROM hakansql
+
+SELECT Sum(Size_new)     --total litre (8)
+FROM (SELECT Country ,CAST(CASE Size WHEN '75CL' THEN '0.75' WHEN '1L' THEN '1' 
+END AS DOUBLE PRECISION) AS Size_new 
+FROM hakansql)f
+
+SELECT Country, Sum(Size_new) AS sumsizes    --total litre of the each country with order (9)
+FROM (SELECT Country, CAST(CASE Size WHEN '75CL' THEN '0.75' WHEN '1L' THEN '1' 
+END AS DOUBLE PRECISION) AS Size_new 
+FROM hakansql)f 
+GROUP BY Country ORDER BY sumsizes DESC;
+
+TRUNCATE TABLE hakansql    --close table
+
